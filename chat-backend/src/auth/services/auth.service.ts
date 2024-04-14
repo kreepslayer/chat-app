ddf7cad72173eb172030c9c20c1acde589ba086e
kreepslayer <Danikblashkevich@gmail.com>
2024-04-14 16:00:33 +0300

    added more roles functional

--- chat-backend/src/auth/services/auth.service.ts
+++ chat-backend/src/auth/services/auth.service.ts
@@ -1,13 +1,13 @@
-import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { from, Observable } from 'rxjs';
-import type { User } from 'src/users/models/user.interfase';
-const bcrypt = require('bcrypt');
+import { Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { from, Observable } from "rxjs";
+import type { User } from "src/users/models/user.interfase";
+const bcrypt = require("bcrypt");
 @Injectable()
 export class AuthService {
   constructor(private readonly jwtService: JwtService) {}
-  generateJWT(payload: User): Observable<string> {
-    return from(this.jwtService.signAsync(payload));
+  generateJWT(user: User): Observable<string> {
+    return from(this.jwtService.signAsync({ user }));
   }
 
   hashPassword(password: string): Observable<string> {
