2429bffb24df08ad58e8516b88d5a29518242732
kreepslayer <Danikblashkevich@gmail.com>
2024-04-29 20:46:54 +0300

    linked front reg and auth to backend + redirect

--- chat-frontend/src/app/components/login/login.component.ts
+++ chat-frontend/src/app/components/login/login.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-  constructor(private authService: AuthService) {}
-  username = '';
-  password = '';
-  login() {
-    this.authService.login(this.username, this.password);
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({
+    userName: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+    this.authService
+      .login(this.loginForm.value)
+      .pipe()
+      .subscribe((data) => {
+        console.log(data);
+        console.log('no errors');
+        this.router.navigate(['/main']);
+      });
+    // .pipe(map( (token) => console.log(token) )); // TODO: redirect to main
   }
 }
--- chat-frontend/src/app/components/register/register.component.ts
+++ chat-frontend/src/app/components/register/register.component.ts
@@ -1,32 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, type OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { FormsModule } from '@angular/forms';
+import {
+  FormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { map } from 'rxjs';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export class RegisterComponent {
-  username: string = '';
-  password: string = '';
-  constructor(private authService: AuthService) {}
-  register() {
-    const passCheck: HTMLInputElement = document.querySelector(
-      '#passCheck'
-    ) as HTMLInputElement;
-    console.log(passCheck.value, this.password);
-    if (passCheck.value == this.password) {
-      this.authService
-        .register(this.username, this.password)
-        .subscribe((data) => {
-          console.log(data); // log server response
-          console.log('no errors');
-        });
-    } else {
-      alert('Passwords do not match');
-      return;
-    }
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup = this.fb.group({
+    userName: [null, [Validators.required, Validators.minLength(4)]],
+    password: [null, [Validators.required, Validators.minLength(8)]],
+    passwordConfirm: [null, [Validators.required, Validators.minLength(8)]],
+  });
+  // = new FormGroup({
+  //   userName: new FormControl('', [Validators.required]),
+  //   password: new FormControl('', [Validators.required]),
+  // });
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log(this.registerForm.value);
+    this.authService
+      .register(this.registerForm.value)
+      .pipe()
+      .subscribe((data) => {
+        console.log(data);
+        console.log('no errors');
+        this.router.navigate(['/login']);
+      });
   }
+  //   this.authService
+  //     .login(this.loginForm.value)
+  //     .pipe()
+  //     .subscribe((data) => {
+  //       console.log(data);
+  //       console.log('no errors');
+  //       this.router.navigate(['/main']);
+  //     });
+  // }
+  // register() {
+  //   const passCheck: HTMLInputElement = document.querySelector(
+  //     '#passCheck'
+  //   ) as HTMLInputElement;
+  //   console.log(passCheck.value, this.password);
+  //   if (passCheck.value == this.password) {
+  //     this.authService
+  //       .register(this.username, this.password)
+  //       .subscribe((data) => {
+  //         console.log(data); // log server response
+  //         console.log('no errors');
+  //       });
+  //   } else {
+  //     alert('Passwords do not match');
+  //     return;
+  //   }
+  // }
   back() {
     window.history.back();
   }
